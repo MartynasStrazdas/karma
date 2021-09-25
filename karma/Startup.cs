aa3624f2b4584098268b1315051427544c299401
Vilius Puškunalis <47086537+Puskunalis@users.noreply.github.com>
2021-09-25 16:05:38 +0300

    Fix deployment connection to database

--- karma/Startup.cs
+++ karma/Startup.cs
@@ -21,7 +21,9 @@ namespace karma
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public static IConfiguration Configuration { get; set; }
+
+        public static string DBConnectionString { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -39,9 +41,11 @@ namespace karma
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                DBConnectionString = Environment.GetEnvironmentVariable("db-connection-string", EnvironmentVariableTarget.Machine);
             }
             else
             {
+                DBConnectionString = Configuration.GetConnectionString("db-karma");
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
