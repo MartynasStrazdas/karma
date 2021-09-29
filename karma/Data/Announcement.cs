65c53fb7bb70556dab8818e5fd4983581b3c7338
Martynas <43886789+MartynasStrazdas@users.noreply.github.com>
2021-09-29 12:11:32 +0300

    Announcement page back end (#35)

---
+++ karma/Data/Announcement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace karma.Data
+{
+    public class Announcement
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Added { get; set; }
+
+        public Announcement(int Id, string Name, string Description, DateTime Added)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Description = Description;
+            this.Added = Added;
+        }
+    }
+}
