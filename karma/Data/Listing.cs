3a69debbce8c23f81b16967f21ec75944fa05872
justasgasparaitis <gasparaiciukas@gmail.com>
2021-09-29 21:39:01 +0300

    Add Announcements & Listings buttons

--- karma/Data/Listing.cs
+++ karma/Data/Listing.cs
@@ -7,17 +7,23 @@ namespace karma.Data
 {
     public class Listing
     {
-            public int Id { get; set; }
-            public string Title { get; set; }
-            public string Description { get; set; }
-            public DateTime Added { get; set; }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Added { get; set; }
 
-            public Listing(int Id, string Title, string Description, DateTime Added)
-            {
-                this.Id = Id;
-                this.Title = Title;
-                this.Description = Description;
-                this.Added = Added;
-            }
+        public Listing(int Id, string Title, string Description, DateTime Added)
+        {
+            this.Id = Id;
+            this.Title = Title;
+            this.Description = Description;
+            this.Added = Added;
+        }
+
+        public Listing(string Title, string Description)
+        {
+            this.Title = Title;
+            this.Description = Description;
+        }
     }
 }
--- karma/Pages/Announcements.razor.cs
+++
@@ -1,6 +0,0 @@
-public class AnnouncementListing
-{
-    public string Title { get; set; }
-    public string Text{ get; set; }
-    public string ImageSource { get; set; }
-}
