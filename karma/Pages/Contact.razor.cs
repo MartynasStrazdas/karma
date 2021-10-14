69415168c8d2c65ccbf685baf91edf5bbd5dc031
Martynas Strazdas <martynas.strazdas@gmail.com>
2021-10-14 20:50:53 +0300

    Add email functionality

--- karma/Pages/Contact.razor.cs
+++ karma/Pages/Contact.razor.cs
@@ -6,11 +6,12 @@ namespace karma.Pages
     public partial class Contact
     {
         private string _subject, _description;
-
+        
         private void sendEmail()
         {
+            DotNetEnv.Env.Load();
             MailMessage mail = new MailMessage("CharieOrganization@gmail.com", "CharieOrganization@gmail.com", _subject, _description);
-            NetworkCredential netCred = new NetworkCredential("CharieOrganization@gmail.com", karma.GitIgnored.EmailPasswordcs.GetEmailPassword());
+            NetworkCredential netCred = new NetworkCredential("CharieOrganization@gmail.com", DotNetEnv.Env.GetString("EMAIL_PASSWORD"));
             SmtpClient smtpobj = new SmtpClient("smtp.gmail.com", 587);
             smtpobj.EnableSsl = true;
             smtpobj.Credentials = netCred;
