e205d7d0c170004a8c8b66f6badc8b5c201eb7c0
Martynas Strazdas <martynas.strazdas@gmail.com>
2021-10-14 10:46:04 +0300

    Separate cs from razor

---
+++ karma/Shared/NavMenu.razor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using karma.Components.Dialogs;
+
+
+namespace karma.Shared
+{
+    public partial class NavMenu
+    {
+        private String MenuButtonClass = "nav-not-pressed"; //Determines <nav> class, to open mobile navbar.
+
+        private void ExpandMenu()
+        {
+            MenuButtonClass = "nav-pressed"; //Changes <nav> class to expand mobile menu.
+        }
+        private void CloseMenu()
+        {
+            MenuButtonClass = "nav-not-pressed"; //Changes <nav> class to close mobile menu.
+        }
+        private async Task OpenDialog()
+        {
+            var dialog = _dialogService.Show<DialogLogin>("Login");
+            var result = await dialog.Result;
+
+            if (!result.Cancelled)
+            {
+
+            }
+        }
+    }
+}
