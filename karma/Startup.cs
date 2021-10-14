6e8e4b78074eed0f4927c4c1de6ce9b94e46d136
Martynas <43886789+MartynasStrazdas@users.noreply.github.com>
2021-10-14 21:32:54 +0300

    Strazdas/send email functionality (#70)
    * Send email functionality
    
    * Testing if gitIgnore works
    
    * Test nr 2
    
    * Add email functionality
    
    * Env file ex, add DB connection to .env
    
    * .ENV.SAMPLE

--- karma/Startup.cs
+++ karma/Startup.cs
@@ -40,11 +40,8 @@ namespace karma
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                DBConnectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
-                if (DBConnectionString == null)
-                {
-                    DBConnectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING", EnvironmentVariableTarget.Machine);
-                }
+                DotNetEnv.Env.Load();
+                DBConnectionString = DotNetEnv.Env.GetString("DB_CONNECTION_STRING");
             }
             else
             {
