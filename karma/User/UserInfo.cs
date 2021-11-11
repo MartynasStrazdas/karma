3c7fd9d2612079083e07f9f80ec7e7e8975e0554
Martynas <43886789+MartynasStrazdas@users.noreply.github.com>
2021-11-11 15:50:36 +0200

    Strazdas/login implementation (#103)
    * Add user class
    
    * Add Registration dialog

---
+++ karma/User/UserInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace karma.User
+{
+    public class UserInfo
+    {
+        private int UserID { get; }
+        string Name { get; }
+        string Surname { get; }
+        string Username { get; }
+        string Email { get; }
+        bool IsUser { get; }
+        bool IsCharity { get; }
+        bool IsAdmin { get; }
+    }
+}
