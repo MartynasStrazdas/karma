647faf041216d27845c23630b4869914500bed9f
Redas <86876763+redasSer@users.noreply.github.com>
2021-12-02 18:58:08 +0200

    Add User page and logout functionality (#112)

---
+++ karma/Pages/User_page.razor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace karma.Pages
+{
+    public partial class User_page
+    {
+
+    }
+}
