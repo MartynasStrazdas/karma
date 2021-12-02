11c49b0879385c7317aadf44dd496067bd19d508
Redas <86876763+redasSer@users.noreply.github.com>
2021-12-02 19:31:59 +0200

    Redas/minor improvements (#113)
    * Add User page and logout functionality
    
    * Revert "Add User page and logout functionality"
    
    This reverts commit 74ee575d99d5370344c272bf67f3df1a211431c5.
    
    * Minor improvements

--- karma/Startup.cs
+++ karma/Startup.cs
@@ -47,7 +47,7 @@ namespace karma
             }
             else
             {
-                DBConnectionString = Configuration.GetConnectionString("db-karma");
+                DBConnectionString = Configuration.GetConnectionString("db_a7d4c3_karma");
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
