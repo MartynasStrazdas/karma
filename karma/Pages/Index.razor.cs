058202ec447c10de24ee4b5795b318d75e4dd119
redasSer <redas.serafim@gmail.com>
2021-12-09 19:40:12 +0200

    Add apply dialog, and some minor changes

--- karma/Pages/Index.razor.cs
+++ karma/Pages/Index.razor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using karma.Components.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,15 @@ namespace karma.Pages
 {
     public partial class Index
     {
+        private async Task OpenDialogRegister()
+        {
+            var dialog = _dialogService.Show<DialogRegister>("Register");
+            var result = await dialog.Result;
+
+            if (!result.Cancelled)
+            {
+
+            }
+        }
     }
 }
