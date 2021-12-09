67aa7ddec1c1519e2ec1d52eb18b95f675913a3d
justasgasparaitis <gasparaiciukas@gmail.com>
2021-12-09 20:29:39 +0200

    Add Registration

---
+++ karma/Data/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace karma
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool IsUser { get; set; }
+        public bool IsCharity { get; set; }
+        public bool IsAdmin { get; set; }
+        public byte[] Img { get; set; }
+    }
+}
---
+++ karma/Data/db_a7d4c3_karmaContext.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace karma
+{
+    public partial class db_a7d4c3_karmaContext : DbContext
+    {
+        public db_a7d4c3_karmaContext()
+        {
+        }
+
+        public db_a7d4c3_karmaContext(DbContextOptions<db_a7d4c3_karmaContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Announcement> Announcements { get; set; }
+        public virtual DbSet<Charity> Charities { get; set; }
+        public virtual DbSet<Listing> Listings { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=SQL5108.site4now.net;Initial Catalog=db_a7d4c3_karma;User Id=db_a7d4c3_karma_admin;Password=slaptazodis123");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Announcement>(entity =>
+            {
+                entity.ToTable("announcements");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Added)
+                    .HasColumnType("datetime")
+                    .HasColumnName("added");
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("description");
+
+                entity.Property(e => e.Img).HasColumnName("img");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("title");
+
+                entity.Property(e => e.ValidUntil)
+                    .HasColumnType("datetime")
+                    .HasColumnName("validUntil");
+            });
+
+            modelBuilder.Entity<Charity>(entity =>
+            {
+                entity.ToTable("charity");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Added)
+                    .HasColumnType("datetime")
+                    .HasColumnName("added");
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("description");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Website)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("website");
+            });
+
+            modelBuilder.Entity<Listing>(entity =>
+            {
+                entity.ToTable("listings");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Added)
+                    .HasColumnType("datetime")
+                    .HasColumnName("added");
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("description");
+
+                entity.Property(e => e.Img).HasColumnName("img");
+
+                entity.Property(e => e.Title)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("title");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Img).HasColumnName("img");
+
+                entity.Property(e => e.IsAdmin).HasColumnName("isAdmin");
+
+                entity.Property(e => e.IsCharity).HasColumnName("isCharity");
+
+                entity.Property(e => e.IsUser).HasColumnName("isUser");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("password");
+
+                entity.Property(e => e.Surname)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("surname");
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasColumnName("username");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
