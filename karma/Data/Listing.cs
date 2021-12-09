f3992c8e0e42286cccabaf1fab7ddf242a8b9c6f
redasSer <redas.serafim@gmail.com>
2021-12-09 18:23:34 +0200

    Add upload listing image

--- karma/Data/Listing.cs
+++ karma/Data/Listing.cs
@@ -11,5 +11,6 @@ namespace karma
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime Added { get; set; }
+        public byte[] Img { get; set; }
     }
 }
