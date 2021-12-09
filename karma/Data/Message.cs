09d028f0815b1ed9781822760dc1b7f08f035dc8
Justas <52386008+JustasGasparaitis@users.noreply.github.com>
2021-12-09 23:23:57 +0200

    Database: Add Message (#123)

---
+++ karma/Data/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace karma
+{
+    public partial class Message
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ListingId { get; set; }
+        public string Message1 { get; set; }
+    }
+}
