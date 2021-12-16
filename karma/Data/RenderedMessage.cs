01ee6c47f466bd57adb29594a2c4df4c25be97c9
redasSer <redas.serafim@gmail.com>
2021-12-16 20:34:37 +0200

    Add LINQ requirements

---
+++ karma/Data/RenderedMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace karma.Data
+{
+    public class RenderedMessage
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public byte[] Img { get; set; }
+        public string Message { get; set; }
+        public string Sender { get; set; }
+        public int ListerId { get; set; }
+
+    }
+}
--- karma/Pages/UserPage.razor.cs
+++ karma/Pages/UserPage.razor.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using karma.Components.Dialogs;
 using MudBlazor;
 using Microsoft.EntityFrameworkCore;
+using karma.Data;
 
 namespace karma.Pages
 {
@@ -15,7 +16,10 @@ namespace karma.Pages
         private List<Listing> _listings;
         private List<Message> _messages;
         private List<User> _users;
-      
+        private IEnumerable<RenderedMessage> renderedMessages;
+        private IEnumerable<IGrouping<bool,User>> charityCount;
+        //private List<RenderedMessage> _renderedMessages;
+
         public User getUserByID(int thisId)
         {
             using (var db = new db_a7d4c3_karmaContext())
@@ -25,6 +29,7 @@ namespace karma.Pages
             }
             
         }
+
         protected override async Task OnInitializedAsync()
         {
             using (var db = new db_a7d4c3_karmaContext())
@@ -32,8 +37,27 @@ namespace karma.Pages
                 _announcements = await db.Announcements.OrderByDescending(x => x.Added).ToListAsync();
                 _listings = await db.Listings.OrderByDescending(x => x.Added).ToListAsync();
                 _messages = await db.Messages.ToListAsync();
+                _users = await db.Users.ToListAsync();
             }
+            //REQUIREMENT 3.10 LINQ JOIN
+            renderedMessages = _messages.Join(
+            _listings,
+            message => message.ListingId,
+            listing => listing.Id,
+            (message, listing) => new RenderedMessage
+            {
+                Title = listing.Title,
+                Description = listing.Description,
+                Img = listing.Img,
+                Message = message.Message1,
+                Sender = getUserByID(message.UserId).Name,
+                ListerId = listing.UserId
+            }) ;
+
+            //REQUIREMENT 3.10 LINQ GROUPBY
+            charityCount = _users.GroupBy(e => e.IsCharity);
         }
+
         async Task OpenDialogConfirmationListing(int Id)
         {
             DialogOptions options = new DialogOptions() { MaxWidth = MaxWidth.Large, FullWidth = true };
